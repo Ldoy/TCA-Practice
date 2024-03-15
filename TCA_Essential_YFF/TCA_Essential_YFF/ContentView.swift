423cc8181ec2913b2ad3ee0d2f8883d77dc3b316
Ldoy <leedoy7989@gmail.com>
2024-03-15 11:38:57 +0900

    docs: Add TCA SPM

---
+++ TCA_Essential_YFF/TCA_Essential_YFF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Essential_YFF
+//
+//  Created by Do Yi Lee on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Essential_YFF/TCA_Essential_YFF/TCA_Essential_YFFApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Essential_YFFApp.swift
+//  TCA_Essential_YFF
+//
+//  Created by Do Yi Lee on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Essential_YFFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
